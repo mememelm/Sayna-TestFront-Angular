fd8e1f49c836e00bdc902f79c29a11a59eb4bc21
mememelm <carol.arivony@gmail.com>
2021-11-13 09:55:58 +0300

    Firebase configuration

---
+++ src/app/constants/firebase-configuration.ts
@@ -0,0 +1,9 @@
+export const FirebaseConfiguration = {
+  apiKey: "AIzaSyCbnAyxy6edYZ6Mk1fZDADSnTVjNelNQH8",
+  authDomain: "sayna-test-angular.firebaseapp.com",
+  projectId: "sayna-test-angular",
+  storageBucket: "sayna-test-angular.appspot.com",
+  messagingSenderId: "1089480151201",
+  appId: "1:1089480151201:web:317ba46f3a79d0fcaf60e0",
+  measurementId: "G-47TG8CT9Y3"
+}

9256f6a0ff504b46f24ca53da5eba42511389d32
mememelm <carol.arivony@gmail.com>
2021-11-13 16:55:14 +0300

    healthy family ok

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -12,12 +12,31 @@ export class HeaderComponent implements OnInit {
 
   logo!: string
   banner!: string
+  healthFamily!: string
+  tick!: string
+
+  listChoice = [
+    {
+      title: 'Comprehensive Insurance',
+      explanation: 'Dynamically repurpose e-business users rather than granular products.'
+    },
+    {
+      title: 'Support is just a call away',
+      explanation: 'Rapidiously customize value-added platforms compliant action items.'
+    },
+    {
+      title: 'Say goodbye to paperwork.',
+      explanation: 'Globally deliver economically sound communities relationships.'
+    }
+  ]
 
   constructor(public ctrl: ControllerService) { }
 
   ngOnInit(): void {
     this.logo = this.ctrl.image(ImageList.logo)
     this.banner = this.ctrl.image(ImageList.banner)
+    this.healthFamily = this.ctrl.image(ImageList.healthFamily)
+    this.tick = this.ctrl.image(ImageList.tick)
   }
 
 }
--- src/app/constants/helpers/images-list.ts
+++ src/app/constants/helpers/images-list.ts
@@ -3,5 +3,6 @@ import { Endpoints } from 'src/app/constants/classes/endpoints';
 export const ImageList = {
   logo: Endpoints.LINK_IMAGE_STORAGE + 'logo.png?alt=media&token=f0b4961f-f7e8-4b79-9bbe-d011a7e76762',
   banner: Endpoints.LINK_IMAGE_STORAGE + 'banner-1.png?alt=media&token=9c7549ab-baa1-4317-84e4-9fe3a2289cf7',
-  healthFamily: Endpoints.LINK_IMAGE_STORAGE + 'health-family-1.png?alt=media&token=c339fb8c-4b1c-4dc3-bb3e-7e9304248bba'
+  healthFamily: Endpoints.LINK_IMAGE_STORAGE + 'health-family-1.png?alt=media&token=c339fb8c-4b1c-4dc3-bb3e-7e9304248bba',
+  tick: Endpoints.LINK_IMAGE_STORAGE + 'tick.png?alt=media&token=4ab78b0a-57e2-4b83-a693-9a0b3a661c88'
 }
